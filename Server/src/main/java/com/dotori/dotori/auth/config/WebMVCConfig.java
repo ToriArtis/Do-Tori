c7e5dc8729d07a6c88cce6d96845a30de9a4722e
Rencal3491 <jmj3491329@naver.com>
2024-08-02 01:51:47 +0900

    🐛 Fix: config, postservice
    Fix: config, postservice

--- Server/src/main/java/com/dotori/dotori/auth/config/WebMVCConfig.java
+++ Server/src/main/java/com/dotori/dotori/auth/config/WebMVCConfig.java
@@ -12,7 +12,7 @@ public class WebMVCConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:8081/")
+                .allowedOrigins("http://localhost:3000")
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true)
