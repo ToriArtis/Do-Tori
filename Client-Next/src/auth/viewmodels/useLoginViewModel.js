9fb0897cf0b2fea42f916334063977cfc44cf6cd
LeeSoHyunn <aralshyun@naver.com>
2024-08-01 23:32:20 +0900

    ✨ Feat : login Add
    Feat : login Add

---
+++ Client-Next/src/auth/viewmodels/useLoginViewModel.js
@@ -0,0 +1,29 @@
+import { useState } from 'react';
+import { login } from "../api/authLoginApi";
+import useForm from '../hooks/useForm'; 
+
+export function useLoginViewModel() {
+  const { values, handleChange } = useForm({
+    email: '',
+    password: '',
+  });
+
+  const [error, setError] = useState(null);
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      await login(values);
+    } catch (error) {
+      setError('로그인에 실패했습니다. 이메일과 비밀번호를 확인해주세요.');
+    }
+  };
+
+  return {
+    email: values.email,
+    password: values.password,
+    handleChange,
+    handleSubmit,
+    error,
+  };
+}
