f0ad21aa97df33836964f11163a52ce1ba621c99
unknown <gusdudco6@naver.com>
2024-08-11 22:46:16 +0900

    🐛 Fix : add
    add

--- Client-Next/src/Config.js
+++
@@ -1,4 +0,0 @@
-export const googleKey = '275940286066-6d9ghbsebiiogqbamr43i282c3iljom4.apps.googleusercontent.com';
-export const naverKey = 'hQNSpfq2RZleCopA_CxZ';
-export const kakaoKey = '20dde04ec522b04f1fe73703a04ba0e';
-export const redirectUri = 'http://localhost:3000/oauth2/redirect';
