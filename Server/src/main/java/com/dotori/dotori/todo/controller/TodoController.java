ab8fa042fc2862c189229286610bc084ab8bd1fa
unknown <gusdudco6@naver.com>
2024-08-11 19:46:08 +0900

    ✅ Test : auth input test
    test : auth input test

--- Server/src/main/java/com/dotori/dotori/todo/controller/TodoController.java
+++ Server/src/main/java/com/dotori/dotori/todo/controller/TodoController.java
@@ -15,7 +15,6 @@ import org.springframework.web.bind.annotation.*;
 import java.security.Principal;
 import java.util.List;
 
-// TodoController.java
 @RestController
 @RequestMapping("/todo")
 @RequiredArgsConstructor
--- Server/src/test/java/com/dotori/dotori/repository/AuthRepositoryTests.java
+++ Server/src/test/java/com/dotori/dotori/repository/AuthRepositoryTests.java
@@ -1,104 +1,93 @@
-//package com.dotori.dotori.repository;
-//
-//import com.dotori.dotori.entity.Auth;
-//import lombok.extern.log4j.Log4j2;
-//import org.junit.jupiter.api.Test;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.test.context.SpringBootTest;
-//import org.springframework.security.crypto.password.PasswordEncoder;
-//import org.springframework.test.annotation.Commit;
-//
-//import java.util.Optional;
-//import java.util.stream.IntStream;
-//
-//@SpringBootTest
-//@Log4j2
-//public class AuthRepositoryTests {
-//
-//    @Autowired
-//    private AuthRepository authRepository;
-//
-//    @Autowired
-//    private PasswordEncoder passwordEncoder;
-//
-//    @Test
-//    public void insertUsers() {
-//
-//        IntStream.range(2, 21).forEach(i -> {
-//            Auth auth = Auth.builder()
-//                    .id("test" + i)
-//                    .password(passwordEncoder.encode("1111"))
-//                    .nickName("tu" + i)
-//                    .email("test" + i + "@naver.com")
-//                    .provider("naver")
-//                    .social(true)
-//                    .tutorialDone(true)
-//                    .build();
-//
-//            authRepository.save(auth);
-//        });
-//    }
-//
-//
-//    @Test
-//    public void testRead(){
-//        Optional<Auth> auth = authRepository.findById("test10");
-//        Auth auth1 = auth.orElseThrow();
-//
-//        log.info(auth1);
-//        log.info(auth1.getAid());
-//
-//        log.info(auth1.getNickName());
-//    }
-//
-//    @Test
-//    public void testByNickName(){
-//        Optional<Auth> auth = authRepository.findByNickName("tu10");
-//        Auth auth1 = auth.orElseThrow();
-//
-//        log.info(auth1);
-//        log.info(auth1.getAid());
-//
-//        log.info(auth1.getNickName());
-//    }
-//
-//    @Test
-//    public void testByEmail(){
-//        Optional<Auth> auth = authRepository.findByEmail("test10@naver.com");
-//        Auth auth1 = auth.orElseThrow();
-//        log.info(auth1);
-//        log.info(auth1.getAid());
-//        log.info(auth1.getNickName());
-//    }
-//
-//    @Test
-//    public void testByProvider(){
-//        Optional<Auth> auth = authRepository.findUserByEmailAndProvider("test10@naver.com", "naver");
-//        Auth auth1 = auth.orElseThrow();
-//
-//        log.info(auth1);
-//        log.info(auth1.getAid());
-//
-//        log.info(auth1.getNickName());
-//    }
-//
-//    @Commit
-//    @Test
-//    public void testUpdate(){
-//        String id = "test10";
-//        String updatePassword = passwordEncoder.encode("test10");
-//        String updateNickName = "test10";
-//        String updateEmail = "test10@naver.com";
-//        authRepository.updateAuth(updatePassword, updateNickName,updateEmail, id);
-//
-//        log.info(authRepository.findById(id).get());
-//    }
-//
-//    @Test
-//    public void testDelete(){
-//        testRead();
-//        String id = "test10";
-//        authRepository.deleteById(id);
-//
-//    }
-//}
+package com.dotori.dotori.repository;
+
+import com.dotori.dotori.auth.entity.Auth;
+import com.dotori.dotori.auth.repository.AuthRepository;
+import lombok.extern.log4j.Log4j2;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.test.annotation.Commit;
+
+import java.util.Optional;
+import java.util.stream.IntStream;
+import java.util.stream.LongStream;
+
+@SpringBootTest
+@Log4j2
+public class AuthRepositoryTests {
+
+    @Autowired
+    private AuthRepository authRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Test
+    public void insertUsers() {
+
+        IntStream.range(2, 21).forEach(i -> {
+            Auth auth = Auth.builder()
+                    .password(passwordEncoder.encode("1111"))
+                    .nickName("tu" + i)
+                    .email("test" + i + "@naver.com")
+                    .provider("naver")
+                    .build();
+
+            authRepository.save(auth);
+        });
+    }
+
+
+    @Test
+    public void testRead(){
+        LongStream.range(2L, 21L).forEach(i -> {
+            Optional<Auth> auth = authRepository.findById(i);
+            Auth auth1 = auth.orElseThrow();
+
+            log.info(auth1);
+            log.info(auth1.getId());
+
+            log.info(auth1.getNickName());
+        });
+    }
+
+    @Test
+    public void testByNickName(){
+        Optional<Auth> auth = authRepository.findByNickName("tu10");
+        Auth auth1 = auth.orElseThrow();
+
+        log.info(auth1);
+        log.info(auth1.getId());
+
+        log.info(auth1.getNickName());
+    }
+
+    @Test
+    public void testByEmail(){
+        Optional<Auth> auth = authRepository.findByEmail("test10@naver.com");
+        Auth auth1 = auth.orElseThrow();
+        log.info(auth1);
+        log.info(auth1.getId());
+        log.info(auth1.getNickName());
+    }
+
+
+    @Commit
+    @Test
+    public void testUpdate(){
+        String id = "test10";
+        String updatePassword = passwordEncoder.encode("test10");
+        String updateEmail = "test10@naver.com";
+        authRepository.updatePassword(updateEmail, updatePassword);
+
+        log.info(authRepository.findByEmail(updateEmail).get());
+    }
+
+    @Test
+    public void testDelete(){
+        testRead();
+        Long id = 10L;
+        authRepository.deleteById(id);
+    }
+}
