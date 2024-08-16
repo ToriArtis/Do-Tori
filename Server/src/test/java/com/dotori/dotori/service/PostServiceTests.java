5dc225b1f01bf043a4a77050131227708e8d780a
unknown <gusdudco6@naver.com>
2024-08-16 11:04:26 +0900

    ✅ Test   : Post Service Test add
    post service test add

--- Server/src/test/java/com/dotori/dotori/service/PostServiceTests.java
+++ Server/src/test/java/com/dotori/dotori/service/PostServiceTests.java
@@ -1,107 +1,112 @@
-//package com.dotori.dotori.service;
-//
-//import com.dotori.dotori.dto.*;
-//import com.dotori.dotori.repository.AuthRepository;
-//import lombok.extern.log4j.Log4j2;
-//import org.junit.jupiter.api.Test;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.test.context.SpringBootTest;
-//import org.springframework.mock.web.MockMultipartFile;
-//import org.springframework.web.multipart.MultipartFile;
-//
-//import java.io.File;
-//import java.io.FileInputStream;
-//import java.io.IOException;
-//import java.util.ArrayList;
-//import java.util.List;
-//import java.util.Optional;
-//
-//@SpringBootTest
-//@Log4j2
-//public class PostServiceTests {
-//
-//    @Autowired
-//    private PostService postService;
-//    @Autowired
-//    private AuthService authService;
-//
-//    //주석 지우면 에러남
-////    @Test
-////    public void testAddPost() throws Exception {
-////        PostDTO postDTO = PostDTO.builder().content("content").aid(1).build();
-////
-////        int pid = postService.addPost(postDTO);
-////
-////        log.info(pid);
-////    }
-//
-//
+package com.dotori.dotori.service;
+
+import com.dotori.dotori.auth.dto.AuthDTO;
+import com.dotori.dotori.auth.service.AuthService;
+import com.dotori.dotori.post.dto.PageRequestDTO;
+import com.dotori.dotori.post.dto.PageResponseDTO;
+import com.dotori.dotori.post.dto.PostDTO;
+import com.dotori.dotori.post.dto.ToriBoxDTO;
+import com.dotori.dotori.post.service.PostService;
+import lombok.extern.log4j.Log4j2;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@SpringBootTest
+@Log4j2
+public class PostServiceTests {
+
+    @Autowired
+    private PostService postService;
+    @Autowired
+    private AuthService authService;
+
+    //주석 지우면 에러남
 //    @Test
-//    public void testGetPost() {
-//        PostDTO post = postService.getPost(56);
-//        log.info(post.getContent());
+//    public void testAddPost() throws Exception {
+//        PostDTO postDTO = PostDTO.builder().content("content").aid(1L).build();
+//
+//        Long pid = postService.addPost(postDTO);
+//
+//        log.info(pid);
 //    }
-//
-//    //주석 지우면 에러남
-////    @Test
-////    public void updateTest() throws Exception {
-////        log.info("updateTest");
-////        PostDTO postDTO = PostDTO.builder().pid(2).content("contenttest2").
-////                aid(1).build();
-////        postService.modifyPost(postDTO);
-////        log.info(postService.getPost(2).getContent());
-////    }
-//
-//
+
+
+    @Test
+    public void testGetPost() {
+        PostDTO post = postService.getPost(56L);
+        log.info(post.getContent());
+    }
+
+    //주석 지우면 에러남
 //    @Test
-//    public void testDeletePost() {
-//        postService.deletePost(101);
+//    public void updateTest() throws Exception {
+//        log.info("updateTest");
+//        PostDTO postDTO = PostDTO.builder().pid(2).content("contenttest2").
+//                aid(1).build();
+//        postService.modifyPost(postDTO);
+//        log.info(postService.getPost(2).getContent());
 //    }
-//
-//    @Test
-//    public void listTest() {
-//        PageRequestDTO pageRequestDTO = PageRequestDTO.builder()
-//                .type("cn")
-//                .keyword("1")
-//                .size(10)
-//                .build();
-//        PageResponseDTO<PostDTO> responseDTO = postService.list(pageRequestDTO);
-//        log.info(responseDTO);
-//    }
-//
-//    @Test
-//    public void testAddToriBox() throws Exception {
-//        log.info("testAddToriBox");
-//
-//        PostDTO postDTO = postService.getPost(1);
-//        AuthDTO authDTO = authService.info("gusdudco6");
-//
-//        ToriBoxDTO toriBoxDTO = ToriBoxDTO.builder()
-//                .pid(postDTO.getPid())
-//                .aid(authDTO.getAid())
-//                .build();
-//        int id = postService.toriBoxPost(toriBoxDTO);
-//        log.info(id);
-//    }
-//
+
+
+    @Test
+    public void testDeletePost() {
+        postService.deletePost(10L);
+    }
+
+    @Test
+    public void listTest() {
+        PageRequestDTO pageRequestDTO = PageRequestDTO.builder()
+                .type("cn")
+                .keyword("1")
+                .size(10)
+                .build();
+        PageResponseDTO<PostDTO> responseDTO = postService.list(pageRequestDTO);
+        log.info(responseDTO);
+    }
+
+    @Test
+    public void testAddToriBox() throws Exception {
+        log.info("testAddToriBox");
+
+        PostDTO postDTO = postService.getPost(1L);
+        AuthDTO.ResponseDTO authDTO = authService.info("gusdudco6");
+
+        ToriBoxDTO toriBoxDTO = ToriBoxDTO.builder()
+                .pid(postDTO.getPid())
+                .aid(authDTO.getId())
+                .build();
+        Boolean id = postService.toriBoxPost(toriBoxDTO);
+        log.info(id);
+    }
+
 //    @Test
 //    public void testCountToriBox(){
 //        log.info("testCountToriBox");
 //        int result = postService.countLikes(1);
 //        log.info(result);
 //    }
-//
-//
-//
-//    @Test
-//    public void testLikeList(){
-//        log.info("testCountToriBox");
-//        int aid = 1;
-//        List<PostDTO> toriBox = postService.toriBoxSelectAll(aid);
-//        for (PostDTO postDTO : toriBox) {
-//            log.info(postDTO.getContent());
-//        }
-//    }
-//
-//
-//}
+
+
+
+    @Test
+    public void testLikeList(){
+        log.info("testCountToriBox");
+        Long aid = 1L;
+        List<PostDTO> toriBox = postService.toriBoxSelectAll(aid);
+        for (PostDTO postDTO : toriBox) {
+            log.info(postDTO.getContent());
+        }
+    }
+
+
+}
